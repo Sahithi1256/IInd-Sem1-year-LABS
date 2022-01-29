2ec8770fe2e110a2fd924ed659592b57ccd60625
Sahithi1256 <94466932+Sahithi1256@users.noreply.github.com>
2022-01-29 18:50:33 +0530

    Create Unary_operator.cpp

---
+++ C++ lab/Experiment-15/15A/Unary_operator.cpp
@@ -0,0 +1,23 @@
+
+2)#include <iostream>     
+using namespace std;     
+class Unary     
+{     
+   private:     
+      int num;     
+   public:     
+       Unary(): num(1){}     
+       void operator ++()         {      
+          num = num+10;      
+       }     
+       void Print() {      
+           cout<<"The Count is: "<<num;      
+       }     
+};     
+int main()     
+{     
+    Unary u;     
+    ++u;       
+    u.Print();     
+    return 0;     
+}     
